dfde430de0b621a46d2a90d0c0c66ef1e2f8508c
Kshitij Varshikar <kshitijvarshikar.99@gmail.com>
2026-07-05 12:09:10 +0530

    Updated project

--- prac.java
+++ prac.java
@@ -1,26 +1,24 @@
 import java.util.*;
 
 class prac {
+    public static int calPow(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
 
-    public static void printArray(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        if (n % 2 == 0) {
+            return calPow(x, n / 2) * calPow(x, n / 2);
+        } else {
+            return calPow(x, n / 2) * calPow(x, n / 2) * x;
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 7, 8, 3, 1, 2 };
-
-        for (int i = 1; i < arr.length; i++) {
-            int current = i;
-            int j = i - 1;
-            while (j >= 0 && current < arr[j]) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            // placement
-            arr[j + 1] = current;
-        }
-        printArray(arr);
+        int x = 2, n = 5;
+        int ans = calPow(x, n);
+        System.out.println(ans);
     }
 }
