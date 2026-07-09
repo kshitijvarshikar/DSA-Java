1e83cd45a6316f6fe2dcc047390e7c2ae000fa39
Kshitij Varshikar <kshitijvarshikar.99@gmail.com>
2026-07-09 20:20:23 +0530

    Updated project

--- prac.java
+++ prac.java
@@ -1,48 +1,41 @@
 import java.util.*;
 
 public class prac {
-    public static void conquer(int arr[], int si, int mid, int ei) {
-        int merged[] = new int[ei - si + 1];
-        int idx1 = si;
-        int idx2 = mid + 1;
-        int x = 0;
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
 
-        while (idx1 <= mid && idx2 <= ei) {
-            if (arr[idx1] <= arr[idx2]) {
-                merged[x++] = arr[idx1++];
-            } else {
-                merged[x++] = arr[idx2++];
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
 
-        while (idx1 <= mid) {
-            merged[x++] = arr[idx1++];
-        }
-
-        while (idx2 <= ei) {
-            merged[x++] = arr[idx2++];
-        }
-
-        for (int i = 0, j = si; i < merged.length; i++, j++) {
-            arr[j] = merged[i];
-        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
     }
 
-    public static void divide(int arr[], int si, int ei) {
-        if (si >= ei) {
-            return;
+    public static void quicksort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quicksort(arr, low, pivot - 1);
+            quicksort(arr, pivot + 1, high);
         }
-        int mid = si + (ei - si) / 2;
-        divide(arr, si, mid);
-        divide(arr, mid + 1, ei);
-        conquer(arr, si, mid, ei);
-
     }
 
     public static void main(String[] args) {
         int arr[] = { 6, 3, 9, 5, 2, 8 };
+
         int n = arr.length;
-        divide(arr, 0, n - 1);
+
+        quicksort(arr, 0, n - 1);
 
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
