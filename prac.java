c67373519358242c1b76645e2377431bf4e123b0
Kshitij Varshikar <kshitijvarshikar.99@gmail.com>
2026-07-09 19:47:30 +0530

    Updated project

--- prac.java
+++ prac.java
@@ -1,94 +1,52 @@
-class prac {
+import java.util.*;
 
-    public boolean isSafe(char[][] board, int row, int col, int number) {
+public class prac {
+    public static void conquer(int arr[], int si, int mid, int ei) {
+        int merged[] = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid + 1;
+        int x = 0;
 
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == (char) (number + '0')) {
-                return false;
+        while (idx1 <= mid && idx2 <= ei) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
+            } else {
+                merged[x++] = arr[idx2++];
             }
         }
 
-        for (int j = 0; j < board.length; j++) {
-            if (board[row][j] == (char) (number + '0')) {
-                return false;
-            }
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
         }
 
-        int sr = 3 * (row / 3);
-        int sc = 3 * (col / 3);
-
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (board[i][j] == (char) (number + '0')) {
-                    return false;
-                }
-            }
+        while (idx2 <= ei) {
+            merged[x++] = arr[idx2++];
         }
 
-        return true;
+        for (int i = 0, j = si; i < merged.length; i++, j++) {
+            arr[j] = merged[i];
+        }
     }
 
-    public boolean helper(char[][] board, int row, int col) {
-        if (row == board.length) { // base case
-            return true;
+    public static void divide(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
         }
+        int mid = si + (ei - si) / 2;
+        divide(arr, si, mid);
+        divide(arr, mid + 1, ei);
+        conquer(arr, si, mid, ei);
 
-        int nrow = 0;
-        int ncol = 0;
-
-        if (col == board.length - 1) {
-            nrow = row + 1;
-            ncol = 0;
-        } else {
-            nrow = row;
-            ncol = col + 1;
-        }
-
-        if (board[row][col] != '.') {
-            if (helper(board, nrow, ncol)) {
-                return true;
-            }
-        } else {
-
-            for (int i = 1; i <= 9; i++) {
-                if (isSafe(board, row, col, i)) {
-                    board[row][col] = (char) (i + '0');
-                    if (helper(board, nrow, ncol))
-                        return true;
-                    else
-                        board[row][col] = '.';
-                }
-            }
-        }
-
-        return false;
-    }
-
-    public void solveSudoku(char[][] board) {
-        helper(board, 0, 0);
     }
 
     public static void main(String[] args) {
-        char[][] board = {
-                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-                { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
-        };
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+        divide(arr, 0, n - 1);
 
-        prac s = new prac();
-        s.solveSudoku(board);
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
