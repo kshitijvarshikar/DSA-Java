f35b101d0966190f846397c7a35760f83621053f
Kshitij Varshikar <kshitijvarshikar.99@gmail.com>
2026-07-11 19:29:25 +0530

    Add README documentation

--- prac.java
+++ prac.java
@@ -1,50 +1,29 @@
-// constructor and their types
+// polymorphism
 
 import java.util.*;
-
-class Pen {
-    String color;
-    String type;
-
-    public void write() {
-        System.out.println("Writing Something");
-    }
-
-    public void getProperty() {
-        System.out.println(this.color);
-        System.out.println(this.type);
-    }
-}
 
 class Student {
     String name;
     int age;
 
-    public void printInfo() {
-        System.out.println(this.name);
-        System.out.println(this.age);
+    public void printInfo(String name) {
+        System.out.println(name);
     }
 
-    Student(Student std2) {
-        this.name = std2.name;
-        this.age = std2.age;
+    public void printInfo(int age) {
+        System.out.println(age);
     }
 
-    Student() {
-
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
     }
 }
 
 public class prac {
     public static void main(String[] args) {
-
-        Student std1 = new Student();
-        std1.name = "kshitij";
-        std1.age = 21;
-        std1.printInfo();
-
-        Student std2 = new Student(std1);
-        std2.printInfo();
-
+        Student s1 = new Student();
+        s1.name = "kshitij";
+        s1.age = 20;
+        s1.printInfo(s1.name);
     }
 }
