b0e2c4d6498ac1405dcebe35da94fd69b13c8d3a
Kshitij Varshikar <kshitijvarshikar.99@gmail.com>
2026-07-22 19:31:59 +0530

    LinkedList Practice

--- main.java
+++ main.java
@@ -1,110 +1,76 @@
-class LL {
 
-    int size;
-
-    LL() {
-        this.size = 0;
-    }
-
-    Node head;
-
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-            size++;
-        }
-    }
-
-    public void addFirst(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        newNode.next = head;
-        head = newNode;
-    }
-
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node curNode = head;
-        while (curNode.next != null) {
-            curNode = curNode.next;
-        }
-        curNode.next = newNode;
-    }
-
-    public void printList() {
-        if (head == null) {
-            System.out.println("List is Empthy");
-            return;
-        }
-        Node curNode = head;
-        while (curNode != null) {
-            System.out.print(curNode.data + " --> ");
-            curNode = curNode.next;
-        }
-        System.out.println("Null");
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    // Reverse --> Iterable
-    public void printRev() {
-        if (head == null || head.next == null) {
-            return;
-        }
-        Node prevNode = head;
-        Node curNode = head.next;
-        while (curNode != null) {
-            Node nextNode = curNode.next;
-            curNode.next = prevNode;
-
-            prevNode = curNode;
-            curNode = nextNode;
-        }
-        head.next = null;
-        head = prevNode;
-    }
-
-    // Reverse --> Recursively
-    public Node reverseRecursive(Node head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        Node newHead = reverseRecursive(head.next);
-        head.next.next = head;
-        head.next = null;
-
-        return newHead;
-
-    }
-}
+import java.util.LinkedList;
 
 public class main {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev; // arrow change
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
+        }
+        return turtle;
+    }
+
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+            if (head == null || head.next == null) {
+                return true;
+            }
+
+            ListNode middle = findMiddle(head); // 1st half ka end
+            ListNode secondHalfStart = reverse(middle.next);
+
+            ListNode firstHalfStart = head;
+            while (secondHalfStart != null) {
+                if (firstHalfStart.val != secondHalfStart.val) {
+                    return false;
+                }
+                firstHalfStart = firstHalfStart.next;
+                secondHalfStart = secondHalfStart.next;
+            }
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
-        LL list = new LL();
-        list.addLast(1);
-        list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
-        list.printList();
 
-        // list.printRev();
-        // list.printList();
+        main obj = new main();
 
-        list.head = list.reverseRecursive(list.head);
-        list.printList();
+        main.ListNode head = obj.new ListNode(1);
+        head.next = obj.new ListNode(2);
+        head.next.next = obj.new ListNode(2);
+        head.next.next.next = obj.new ListNode(1);
+
+        main.Solution sol = obj.new Solution();
+
+        boolean result = sol.isPalindrome(head);
+
+        System.out.println(result);
 
     }
 }
